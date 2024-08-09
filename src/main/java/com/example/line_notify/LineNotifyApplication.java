2dbbdf7e24459687282897ca9de7275a4a02a492
Oswin Chen 陳睿泰 <oswinchen@jubo.health>
2024-08-09 23:22:01 +0800

    first push

---
+++ src/main/java/com/example/line_notify/LineNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.line_notify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineNotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/line_notify/LineNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.line_notify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineNotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
